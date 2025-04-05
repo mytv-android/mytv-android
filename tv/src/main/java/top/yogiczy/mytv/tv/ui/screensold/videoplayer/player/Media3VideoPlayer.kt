1db85cf33704ab5c907fa7c13b90dd1413fc6031
ldm2060 <ldm200026@live.cn>
2025-04-05 13:30:38 +0800

    改进字幕检测

--- tv/src/main/java/top/yogiczy/mytv/tv/ui/screensold/videoplayer/player/Media3VideoPlayer.kt
+++ tv/src/main/java/top/yogiczy/mytv/tv/ui/screensold/videoplayer/player/Media3VideoPlayer.kt
@@ -391,7 +391,10 @@ class Media3VideoPlayer(
                     List(group.mediaTrackGroup.length) { trackIndex ->
                         group.mediaTrackGroup
                             .getFormat(trackIndex)
-                            // .takeIf { it.roleFlags == C.ROLE_FLAG_SUBTITLE }
+                            .takeIf { 
+                                (it.roleFlags and C.ROLE_FLAG_SUBTITLE != 0) || 
+                                (it.roleFlags and C.ROLE_FLAG_CAPTION != 0) 
+                            }
                             ?.toSubtitleMetadata()
                             ?.copy(isSelected = group.isTrackSelected(trackIndex))
                     }
