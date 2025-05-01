53f23cf39589c098f458edb7f078b87191c10d58
ldm2060 <ldm200026@live.cn>
2025-05-01 22:50:51 +0800

    fix mini bug 拼写错误

--- core/data/src/main/java/top/yogiczy/mytv/core/data/utils/Constants.kt
+++ core/data/src/main/java/top/yogiczy/mytv/core/data/utils/Constants.kt
@@ -18,7 +18,7 @@ object Constants {
      * 应用 代码仓库
      */
     const val ORIGIN_APP_REPO = "https://github.com/yaoxieyoulei/mytv-android"
-    const val APP_REPO = "hhttps://github.com/mytv-android/mytv-android"
+    const val APP_REPO = "https://github.com/mytv-android/mytv-android"
 
     /**
      * 交流群 telegram
